a83f26ec90d5bc063b9dafcbd8d41f78d44ae4c5
Ayush Juvekar <ayushjuvekar@gmail.com>
2024-11-20 18:53:48 -0500

    temporarily remove querying camps

--- src/app/camps/page.tsx
+++ src/app/camps/page.tsx
@@ -1,11 +1,3 @@
-import { db } from "~/server/db";
-
-export default async function Page() {
-  const posts = await db.query.posts.findMany();
-
-  if (posts.length === 0) {
-    return <div>No camps found</div>;
-  }
-
-  return posts.map((post) => <div key={post.id}>{post.title}</div>);
+export default function Page() {
+  return <div>Camps</div>;
 }
